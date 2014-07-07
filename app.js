57b506595d9213fa2fad4fd0ded068f4b5f3b079
Steven Lu <tacticalazn@gmail.com>
2014-07-07 15:33:27 -0400

    Using LESS instead.

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
