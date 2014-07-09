68a30892d31030b62f0276a756faaa24c818eb78
Steven Lu <tacticalazn@gmail.com>
2014-07-09 15:35:34 -0400

    Updating color scheme and logout

--- routes/logout.js
+++ routes/logout.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  req.session.destroy(function(err) {
-    if (err) return next(err);
-    req.flash('info', 'Successfully logged out.');
-    res.redirect('/login');
-  });
+  req.session.uid = null;
+  req.flash('info', 'Successfully logged out.');
+  res.redirect('/login');
 });
 
 module.exports = router;
