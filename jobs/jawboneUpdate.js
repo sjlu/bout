660374b01173d55f37a3b3e7a697900b22164752
Steven Lu <tacticalazn@gmail.com>
2014-07-09 23:29:08 -0400

    Process jawbone callbacks.

---
+++ jobs/jawboneUpdate.js
@@ -0,0 +1,34 @@
+var models = require('../models');
+var jawbone = require('../lib/jawbone');
+var moment = require('moment');
+var async = require('async');
+
+module.exports = function(job, done) {
+  var jawbone_id = job.data.jawbone_id;
+  var timestamp = job.data.timestamp;
+  var start = moment(timestamp*1).startOf('day').format('X');
+  var end = moment(timestamp*1).add(1, 'day').startOf('day').format('X');
+
+  console.log("processing jawbone update", jawbone_id, start, end);
+
+  async.waterfall([
+    function(cb) {
+      models.User.findOne({
+        jawbone_id: jawbone_id
+      }, cb);
+    },
+    function(user, cb) {
+      jawbone.getSteps(start, end, user, function(err, data) {
+        async.each(data, function(day, cb) {
+          models.Activity.findOrCreate(user, day.date, function(err, act) {
+            if (day.steps) {
+              act.steps = day.steps;
+            }
+            act.save(cb);
+          });
+        }, cb);
+      });
+    }
+  ], done);
+
+}
--- routes/callbacks.js
+++ routes/callbacks.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var kue = require('../lib/kue');
+var async = require('async');
 
 router.get('/withings', function(req, res, next) {
   res.send("OK");
@@ -24,12 +25,27 @@ router.post('/withings', function(req, res, next) {
     end_date = date;
   }
 
+  console.log("received callback from withings for:", withings_id);
+
   var query = req.query;
   kue.create('withingsUpdate', {
     withings_id: withings_id,
     start_date: start_date,
     end_date: end_date
   }).save(function(err) {
+    if (err) return next(err);
+    res.send("OK");
+  });
+});
+
+router.post('/jawbone', function(req, res, next) {
+  var body = req.body;
+  async.each(body.events, function(evt, cb) {
+    kue.create('jawboneUpdate', {
+      jawbone_id: evt.user_xid,
+      timestamp: evt.timestamp
+    });
+  }, function(err) {
     if (err) return next(err);
     res.send("OK");
   });
