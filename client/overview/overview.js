01db28b0d8ea42a92cbab3386f3a837f32a4af90
Steven Lu <tacticalazn@gmail.com>
2014-07-10 23:11:16 -0400

    Give today a little more snazz

--- client/overview/overview.js
+++ client/overview/overview.js
@@ -1,8 +1,8 @@
 bout.controller('overview', function($scope, DataManager) {
   DataManager.observe('activity', function(data) {
     $scope.activity = _.map(data, function(day) {
-      day.date = moment(day.date, 'YYYYMMDD').calendar();
-      day.date = day.date.replace(/ at.*/, '');
+      day.displayDate = moment(day.date, 'YYYYMMDD').calendar();
+      day.displayDate = day.displayDate.replace(/ at.*/, '');
       return day;
     });
   });
