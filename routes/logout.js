36a3ca49c3120513d8a022ba609e35685e67117f
Steven Lu <tacticalazn@gmail.com>
2014-07-10 21:04:21 -0400

    Logout goes to the homepage.

--- routes/logout.js
+++ routes/logout.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   req.session.uid = null;
   req.flash('info', 'Successfully logged out.');
-  res.redirect('/login');
+  res.redirect('/');
 });
 
 module.exports = router;
