732e48eff270206bb4f80f976dddccbca3f7d8da
Steven Lu <tacticalazn@gmail.com>
2014-07-14 09:56:38 -0400

    reformatted fulltext search on username

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../../models');
+var _ = require('lodash');
 
 router.get('/search', function(req, res, next) {
   if (!req.query.query) {
@@ -9,6 +10,15 @@ router.get('/search', function(req, res, next) {
 
   models.User.textSearch(req.query.query, function(err, output) {
     if (err) return next(err);
+
+    output = _.map(output.results, function(user) {
+      return {
+        score: user.score,
+        username: user.obj.username,
+        _id: user.obj.id
+      }
+    });
+
     res.json(output);
   });
 });
