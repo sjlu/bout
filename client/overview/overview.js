bb23758a17a71ad4cb907f71e71c073e16084c94
Steven Lu <tacticalazn@gmail.com>
2014-07-17 23:04:53 -0400

    Date interpretation issue.

--- client/overview/overview.js
+++ client/overview/overview.js
@@ -1,6 +1,7 @@
 bout.controller('overview', function($scope, DataManager) {
   DataManager.observe('stats', function(data) {
     $scope.stats = data;
+    console.log($scope.stats);
   });
   DataManager.methods.stats.get();
 
