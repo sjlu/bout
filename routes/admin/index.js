15e0034c9eefac8cb73b454e39f9100d0d36b1e0
Steven Lu <tacticalazn@gmail.com>
2014-08-09 00:21:29 -0400

    Endpoint to get GITREV.

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var middlewares = require('../../middlewares');
+var config = require('../../lib/config');
 
 router.use(middlewares.requiresLogin);
 router.use(middlewares.requiresAdmin);
 
 router.use('/email', require('./email'));
+router.use('/version', function(req, res, next) {
+  res.json({
+    version: config.GITREV
+  });
+});
 
 module.exports = router;
